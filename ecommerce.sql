48a22b31db0d83bf68692e589400d03c35544235
fichot_l <lysafichot@hotmail.fr>
2016-05-15 18:20:51 +0200

    bdd

--- ecommerce.sql
+++ ecommerce.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 12, 2016 at 11:18 AM
+-- Generation Time: May 15, 2016 at 06:19 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -36,6 +36,30 @@ CREATE TABLE IF NOT EXISTS `products` (
   `status` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`),
+  UNIQUE KEY `UNIQ_8D93D649F85E0677` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`, `username`) VALUES
+(1, 'lysafichot@hotmail.fr', '+ivZNRK0hn5LvscqZJbWUnaGetffZY9ARyHkiBlKdnsAkmJRWOdFb0lSJJz3tZcZawUmHYm0On+aTecQSRX1zA==', 'u'),
+(2, 'lysafichot@hotKmail.fr', 'vLMDkTpY4KZ9DVQet4T/P8N/yIqRoMiJR1PRqJOZgs3HRun9aS7cZ5fq5eKw2jGY1lZkgrrrzwKcKAoJd67G/g==', 'lo');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
